acf5573db61419641445ad9a2d34d90f5ee59a15
Rohit R. Sharma <53270138+rohitrsharma3@users.noreply.github.com>
2022-08-06 15:01:57 +0530

    Added updated UI layout via upload
    Includes - Animated Intro Screen & Language Selection Screen

--- main.dart
+++ main.dart
@@ -1,7 +1,10 @@
+import 'dart:ui';
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:highlight_text/highlight_text.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:animated_text_kit/animated_text_kit.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -18,179 +21,264 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const SpeechScreen(),
+      home: FirstScreen(),
     );
   }
 }
 
-class SpeechScreen extends StatefulWidget {
-  const SpeechScreen({Key? key}) : super(key: key);
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
 
   @override
-  State<SpeechScreen> createState() => _SpeechScreenState();
+  State<FirstScreen> createState() => _FirstScreenState();
 }
 
-class _SpeechScreenState extends State<SpeechScreen> {
-   stt.SpeechToText _speech = stt.SpeechToText();
-  bool _isListening = false;
-  String _text = "Press the button to start speaking";  
-  double _confidence = 1.0;
-  String langId = 'en-US';
-  static late String savedWords;
-  @override
-  void initState(){
-    super.initState();
-    _speech = stt.SpeechToText();
-  }
+class _FirstScreenState extends State<FirstScreen> with TickerProviderStateMixin {
+  final Shader linearGradient = LinearGradient(
+    colors: <Color>[Color.fromARGB(255, 42, 69, 225), Color.fromARGB(255, 119, 72, 212)]
+    //colors: <Color>[Color.fromARGB(255, 28, 52, 185), Color.fromARGB(255, 168, 148, 209)],
+  ).createShader(Rect.fromLTWH(0.0, 0.0, 200.0, 70.0));
 
+  late final AnimationController _controller = AnimationController(duration: const Duration(seconds: 2), vsync: this)
+    ..repeat(reverse: true);
+
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Confidence : ${(_confidence * 100).toStringAsFixed(1)}'),
-        actions: [
-          PopupMenuButton<String>(itemBuilder: (context) =>[
-            PopupMenuItem(value: 'en-US', 
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                Text("en-US")
-              ],
-            )
-            ),
-            PopupMenuItem(value: 'en-GB', 
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                Text("en-UK")
-              ],
-            )
-            ),
-            PopupMenuItem(value: 'en-AUS', 
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                Text("en-AU")
-              ],
-            )
-            ),
-            PopupMenuItem(value: 'hi-IN', 
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                Text("Hindi")
-              ],
-            )
-            ),
-            PopupMenuItem(value: 'ta-IN', 
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                Text("Tamil")
-              ],
-            )
-            ),
-            PopupMenuItem(value: 'te-IN', 
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                Text("Telugu")
-              ],
-            )
-            ),
-            PopupMenuItem(value: 'mr-IN', 
-            child: Row(
-              children: [
-                SizedBox(width: 10),
-                Text("Marathi")
-              ],
-            )
-            )
-          ],
-          onSelected: (value) => {
-            langId = value
-          })
-        ],
-      ),
-      floatingActionButtonLocation : FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton(
-      onPressed: _listen,
-      child : Icon(_isListening ? Icons.mic : Icons.mic_none),
-      
-      ),
-      
-      body: SingleChildScrollView(reverse: true,child: Container(
-        padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 150.0),
-        child: Text(_text),
+        //backgroundColor: Colors.blue[100],
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('Reading Reparo'),
+          backgroundColor: Color.fromARGB(255, 13, 38, 165),
         ),
-      ),
+        body: Wrap(
+          children: [
+            Column(
+              children: [
+                  Container(
+              padding: EdgeInsets.fromLTRB(103, 280, 91, 472),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TyperAnimatedText(
+                      "Reading Reparo",
+                      textStyle:
+                          TextStyle(fontSize: 30, 
+                          foreground: Paint()..shader = linearGradient, 
+                          fontFamily: "Jeju-Gothic",
+                          fontWeight: FontWeight.bold),
+                    ),
+                    TyperAnimatedText("Let's Repair Our Reading",
+                        textStyle: TextStyle(
+                            fontSize: 30, 
+                            foreground: Paint()..shader = linearGradient, 
+                            fontFamily: "Jeju-Gothic",
+                        )
+                            ),
+                  ],
+                  repeatForever: true,
+                  )
+                ),
+
+              ],
+            ),
+            
+          ],
+        ),
+        persistentFooterButtons: [
+          Container(
+            child:   ElevatedButton(
+              onPressed: () {
+                Navigator.push(context, 
+                MaterialPageRoute(builder: (context) =>
+                SecondRoute()));
+              },
+              child:  Text("Let's Start"),
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.blue[800]),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)
+                  ),
+                ),
+                fixedSize: MaterialStateProperty.all(
+                    const Size(244, 48)
+                )
+
+              ),
+              ),
+              padding: EdgeInsets.only(right: 66, bottom: 50),
+          )
+        ],
       
+        
+        );
+            
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+   SecondRoute({Key? key}) : super(key: key);
+  final List<String> langs = ["English", 'Hindi', 'Tamil', 'Telugu'];
+  static late String languageSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("New screen"),
+        backgroundColor: Colors.indigo.shade800,
+      ),
+      body: ListView(children: [
+        Column( //Column of languages
+            children: [ 
+              ElevatedButton(onPressed: () {
+
+                  languageSelected = 'en-US';
+                   Navigator.push(  
+                      context,  
+                      MaterialPageRoute(builder: (context) => ThirdRoute()),  
+                    );  
+                }, 
+                
+                child: Row( // Row of language name and image
+                    children: [
+                      Container( // Text Container
+                        child: Text('English',
+                        style: 
+                        TextStyle(
+                          fontSize: 18,
+                          color: Colors.white70
+                        ),),
+                    
+                      ),
+                      Container( // Image Container
+                          child : Image.asset("assets/united-kingdom-flag-icon.png",
+                          width: 35,
+                          height: 26,),
+                          padding: EdgeInsets.only(left:250),
+                      )
+                    ],
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+
+            ElevatedButton(onPressed: () {
+                  languageSelected = 'hi-IN';
+
+            }, child: Row( // Row of language name and image
+              children: [
+                Container( // Text Container
+                  child: Text('Hindi',
+                  style: 
+                  TextStyle(
+                    fontSize: 18,
+                    color: Colors.white70
+                  ),),
+                  padding: EdgeInsets.only(left: 19),
+                ),
+                Container( // Image Container
+                    child : Image.asset("assets/united-kingdom-flag-icon.png",
+                    width: 35,
+                    height: 26,),
+                    padding: EdgeInsets.only(left:250),
+                )
+              ], // Elevated Button Children ends here
+            ),
+            )
+            ,
+            SizedBox(
+                height: 10,
+              ),
+
+            ElevatedButton(onPressed: () {
+                  languageSelected = 'ta-IN';
+
+            }, child: Row( // Row of language name and image
+              children: [
+                Container( // Text Container
+                  child: Text('Tamil',
+                  style: 
+                  TextStyle(
+                    fontSize: 18,
+                    color: Colors.white70
+                  ),),
+                  padding: EdgeInsets.only(left: 19),
+                ),
+                Container( // Image Container
+                    child : Image.asset("assets/united-kingdom-flag-icon.png",
+                    width: 35,
+                    height: 26,),
+                    padding: EdgeInsets.only(left:250),
+                )
+              ], // Elevated Button Children ends here
+            ),
+            ),
+            SizedBox(
+                height: 10,
+              ),
+
+            ElevatedButton(onPressed: () {
+                  languageSelected = 'te-IN';
+
+            }, child: Row( // Row of language name and image
+              children: [
+                Container( // Text Container
+                  child: Text('Telugu',
+                  style: 
+                  TextStyle(
+                    fontSize: 18,
+                    color: Colors.white70
+                  ),),
+                  padding: EdgeInsets.only(left: 19),
+                ),
+                Container( // Image Container
+                    child : Image.asset("assets/united-kingdom-flag-icon.png",
+                    width: 35,
+                    height: 26,),
+                    padding: EdgeInsets.only(left:250),
+                )
+              ], // Elevated Button Children ends here
+            ),
+            )
+            
+          ], // Column children ends here
+        )
+      ], // List view children ends here
+      )
+        
+    );
+  }
+}
+
+class ThirdRoute extends StatefulWidget {
+   ThirdRoute({Key? key}) : super(key: key);
+  String language =  SecondRoute.languageSelected;
+  @override
+  State<ThirdRoute> createState() => _ThirdRouteState();
+}
+
+class _ThirdRouteState extends State<ThirdRoute> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(SecondRoute.languageSelected),
+      ),
     );
     
   }
-
-
- void _listen() async {
-
-    if (!_isListening) {
-      bool available = await _speech.initialize(
-        onStatus: (val) => print('onStatus: $val to ${langId}'),
-        onError: (val) => print('onError: $val'),
-      );
-      if (available) {
-        setState(() => _isListening = true);
-        _speech.listen(
-          onResult: (val) => setState(() {
-            _text = val.recognizedWords;
-            if (val.hasConfidenceRating && val.confidence > 0) {
-              _confidence = val.confidence;
-            }
-          }),
-          localeId: langId,
-        );
-      }
-    } else {
-      setState(() => _isListening = false);
-       FlutterClipboard.copy(_text);
-       savedWords = _text;
-      _speech.stop();
-      Navigator.push(context, MaterialPageRoute(builder: (context) => const AssementRoute()));
-      
-    }
-  }
-
 }
-
-class AssementRoute extends StatelessWidget{
-    const AssementRoute({super.key});
-  
-    @override
-
-    Widget build(BuildContext context){
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Assesement'),
-          actions: [
-          
-            FloatingActionButton(
-              onPressed: () {
-              Navigator.pop(context, MaterialPageRoute(builder: (context) => const SpeechScreen()));
-            },
-            child: Icon(Icons.arrow_back_ios_new),
-            ),
-            
-          ],
-
-        ),
-        body: Center(
-          child: Container(
-            margin: const EdgeInsets.all(10.0),
-            
-            child: Text(_SpeechScreenState.savedWords),
-          ),
-        )
-      );
-    }
-
-  }
